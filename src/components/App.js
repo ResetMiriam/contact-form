d99b5720daf466ef8fcba488b1a72d09250d50bc
ResetMiriam <miriam.verduf@gmail.com>
2021-11-29 09:24:54 +0100

    fix structure and background

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,23 @@ import "../styles/App.scss";
 
 function App() {
   return (
-    <div>
-      <h1>Hola Mundo</h1>
-    </div>
+    <>
+      <div className="block__container--background">
+        <section className="scroll__container">
+          <div className="name__container">
+            <form className="name__form">
+              <label for="name">Escribe un nombre:</label>
+              <input
+                type="name"
+                name="name"
+                id="name"
+                placeholder="Escribe aquí tu respuesta..."
+              />
+            </form>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
 
